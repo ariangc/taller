43a8addc444bd3148dca353d5cd0b5b4f0d246ac
alulab14 <alulab14@inf.pucp.edu.pe>
2018-04-16 19:39:27 -0500

    arian

--- Taller/Taller/Program.cs
+++ Taller/Taller/Program.cs
@@ -17,6 +17,14 @@ namespace Taller
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            int x = sumar(4, 3);
+            System.Console.WriteLine(x);
+        }
+
+        static int sumar(int a, int b)
+        {
+            System.Console.WriteLine("Arian!");
+            return a + b;
         }
     }
 }
